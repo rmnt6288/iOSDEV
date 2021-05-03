0c375a417052eb36494a9ddd95af95a0262b92db
rmnt6288 <rmnt6288@gmail.com>
2021-05-03 20:55:50 +0900

    add CLLocation examples

--- NavigationTest/NavigationTest/DeepView.swift
+++ NavigationTest/NavigationTest/DeepView.swift
@@ -5,13 +5,38 @@
 //  Created by 쿰스 on 2021/05/02.
 //
 import UIKit
+import CoreLocation
 
-
-class DeepView: UIViewController {
+class DeepView: UIViewController, CLLocationManagerDelegate {
+    
+    private var locationManager:CLLocationManager?
+    
+    
+    private var GeoLabel:UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemFill
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 26)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        GeoLabel.frame = CGRect(x: 20, y: view.bounds.height/2-50, width: view.bounds.width-40, height: 100)
+        view.addSubview(GeoLabel)
+        locationManager = CLLocationManager()
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.startUpdatingLocation()
+        locationManager?.delegate = self
         print ("Deep View loaded !!!")
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print ("Location is Updated !!!")
+        if let location = locations.last {
+            GeoLabel.text = "LAT : \(location.coordinate.latitude) \nLNG : \(location.coordinate.longitude)"
+        }
+    }
 }
